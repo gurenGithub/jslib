94bbb921fb94ae1839e38f857ecd0146e7a5da16
guren <guren_d@hotmail.com>
2016-09-01 14:03:05 +0800

    1
    1

--- navDropdown/index.js
+++ navDropdown/index.js
@@ -1,12 +1,9 @@
 
+var  xNavDropdown =(function(){
 
-function xNavDropdown() {
-	// body...
-}
-
-xNavDropdown.prototype.render=function(selector){
-
-if(!selector){
+  var member={
+    render:function(selector){
+     if(!selector){
            selector='xNavDropdown';
          }
         var selectors= document.getElementsByClassName(selector);
@@ -20,19 +17,19 @@ if(!selector){
 
                    for (var j = 0; j < _lis.length; j++) {
                       (function(j){
-                   	      var _li=_lis[j];
+                          var _li=_lis[j];
 
-                   	      var __a=_li.children[0];
-                   	      var __ul=_li.children[1];
+                          var __a=_li.children[0];
+                          var __ul=_li.children[1];
                           _li.style.height=__a.offsetHeight+'px';
-                   	      xUtils.addEvent(_li,"mouseover",function(){
-                   	      	__ul.style.display='block';
-                   	      	__a.className=	__a.className.replace('xNavDropdown-item-hover','')+" xNavDropdown-item-hover";
-                   	      });
-                   	      xUtils.addEvent(_li,"mouseout",function(){
-                   	      	__ul.style.display='none';
-                   	      	__a.className=	__a.className.replace('xNavDropdown-item-hover','')
-                   	      });
+                          xUtils.addEvent(_li,"mouseover",function(){
+                            __ul.style.display='block';
+                            __a.className=  __a.className.replace('xNavDropdown-item-hover','')+" xNavDropdown-item-hover";
+                          });
+                          xUtils.addEvent(_li,"mouseout",function(){
+                            __ul.style.display='none';
+                            __a.className=  __a.className.replace('xNavDropdown-item-hover','')
+                          });
                       })(j)
                    }
                    
@@ -40,11 +37,12 @@ if(!selector){
 
             })(k);   
            }
-}
-
+    }
+  }
+  return member;
+})()
 
 
 xUtils.addEvent(window,'load',function(){
-  var _xNavDropdown=new xNavDropdown();
-      _xNavDropdown.render();
+      xNavDropdown.render();
 })
