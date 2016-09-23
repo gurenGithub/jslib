c17a5819a38c71badaee089b43cb0b22ca8ceb92
guren <guren_d@hotmail.com>
2016-09-23 14:16:47 +0800

    1
    1

--- pager/V1/index.js
+++ pager/V1/index.js
@@ -1,14 +1,16 @@
-function createPager(currentPage, maxPager, onChange, pagerContainer,isMinClick) {
+function createPager(currentPage, maxPager, onChange, pagerContainer, isMinClick) {
 
-    if(!onChange){
-        onChange=function(currentPage){
+    if (!onChange) {
+        onChange = function(currentPage) {
 
         };
     }
     pagerContainer.empty();
+
     function pVisible(item, isV) {
         isV ? item.show() : item.hide();
     }
+
     function addActive(li) {
         li.addClass('active');
         li.siblings().removeClass('active');
@@ -18,18 +20,19 @@ function createPager(currentPage, maxPager, onChange, pagerContainer,isMinClick)
 
         return $('.active', pagerContainer);
     }
+
     function getActivePage(pagerContainer) {
 
         return parseInt(getActive(pagerContainer).attr('value'));
     }
     (
 
-        function (currentPage, maxPager, onChange, pagerContainer, isMinClick) {
+        function(currentPage, maxPager, onChange, pagerContainer, isMinClick) {
             var $ul = $('<ul class="pager"></ul>');
             var $pre = $('<li class="pre">上一页</li>');
             var $next = $('<li class="next">下一页</li>');
             var $first = $('<li class="first" value="1">1</li>');
-            var $max = $('<li class="max" value="'+maxPager+'">'+maxPager+'</li>');
+            var $max = $('<li class="max" value="' + maxPager + '">' + maxPager + '</li>');
             var $np1 = $('<li>...</li>');
             var $np2 = $('<li>...</li>');
 
@@ -39,18 +42,16 @@ function createPager(currentPage, maxPager, onChange, pagerContainer,isMinClick)
 
 
             var prePage = isMinClick ?
-                (currentPage == 2 ? (currentPage) : (currentPage == maxPager - 1 ? maxPager - 3 : (currentPage - 1)))
-                :
-                (maxPager == currentPage ? (currentPage - 3): currentPage + 1);
-            var nextPagt =
+                (currentPage == 2 ? (currentPage) : (currentPage == maxPager - 1 ? maxPager - 3 : (currentPage - 1))) :
+                (maxPager == currentPage ? (currentPage - 3) : currentPage + 1);
+            var nextPage =
                 isMinClick ?
-                ((currentPage == 2 ? (currentPage + 2) : (currentPage == maxPager - 1 ? currentPage : (currentPage + 1))))
-                :
+                ((currentPage == 2 ? (currentPage + 2) : (currentPage == maxPager - 1 ? currentPage : (currentPage + 1)))) :
                 ((maxPager == currentPage ? (currentPage - 1) : currentPage + 3));
             pVisible($np1, prePage > 2);
-            pVisible($np2, nextPagt < (maxPager - 1));
+            pVisible($np2, nextPage < (maxPager - 1));
 
-            for (var i = prePage ; i <= nextPagt ; i++) {
+            for (var i = prePage; i <= nextPage; i++) {
 
                 if (i <= 1) {
                     continue;
@@ -62,7 +63,7 @@ function createPager(currentPage, maxPager, onChange, pagerContainer,isMinClick)
                 $li.html(i);
                 $li.attr('value', i);
                 $ul.append($li);
-                $li.click(function () {
+                $li.click(function() {
                     var value = parseInt($(this).attr('value'));
                     var _currentPage = getActivePage(pagerContainer);
                     if (value == _currentPage) {
@@ -70,14 +71,14 @@ function createPager(currentPage, maxPager, onChange, pagerContainer,isMinClick)
                     }
                     createPager(value, maxPager, onChange, pagerContainer, true);
                     onChange(value, getActive(pagerContainer));
-                    
+
                 });
             }
             $ul.append($np2);
             if (maxPager > 1) {
                 $ul.append($max);
             }
-            $max.click(function () {
+            $max.click(function() {
                 var _currentPage = getActivePage(pagerContainer);
                 if (_currentPage == maxPager) {
                     return;
@@ -86,8 +87,7 @@ function createPager(currentPage, maxPager, onChange, pagerContainer,isMinClick)
                 onChange(maxPager, getActive(pagerContainer));
             });
 
-            $first.click(function ()
-            {
+            $first.click(function() {
                 var _currentPage = getActivePage(pagerContainer);
                 if (_currentPage == 1) {
                     return;
@@ -97,8 +97,7 @@ function createPager(currentPage, maxPager, onChange, pagerContainer,isMinClick)
                 onChange(1, getActive(pagerContainer));
             });
 
-            $pre.click(function ()
-            {
+            $pre.click(function() {
                 var activeIndex = getActivePage(pagerContainer);
                 var _currentPage = activeIndex - 1;
                 if (activeIndex <= 1) {
@@ -107,7 +106,7 @@ function createPager(currentPage, maxPager, onChange, pagerContainer,isMinClick)
                 createPager(_currentPage, maxPager, onChange, pagerContainer, _currentPage == 1 ? false : true);
                 onChange(_currentPage, getActive(pagerContainer));
             });
-            $next.click(function () {
+            $next.click(function() {
                 var activeIndex = getActivePage(pagerContainer);
                 var _currentPage = activeIndex + 1;
                 if (activeIndex >= maxPager) {
@@ -118,19 +117,25 @@ function createPager(currentPage, maxPager, onChange, pagerContainer,isMinClick)
             });
             $ul.append($next);
             pagerContainer.append($ul);
-           
+
             addActive($('li[value=' + currentPage + ']', pagerContainer));
-        })(currentPage, maxPager, onChange, pagerContainer,isMinClick);
+        })(currentPage, maxPager, onChange, pagerContainer, isMinClick);
 }
 
 
-var xPager=(function(){
+var xPager = (function() {
 
-  var members={
+    var members = {
 
-      render:function(pagerContainer,currentPage, maxPager, onChange ){
-             createPager(currentPage, maxPager, onChange, pagerContainer);
-      }
-  }
-  return members;
+        /*
+         @pagerContainer jq对象
+         @currentPage    当前页数
+         @maxPager       最大页数
+         @onChange       页面变化事件function(currentPageIndex,$me){}
+        */
+        render: function(pagerContainer, currentPage, maxPager, onChange) {
+            createPager(currentPage, maxPager, onChange, pagerContainer);
+        }
+    }
+    return members;
 })();
