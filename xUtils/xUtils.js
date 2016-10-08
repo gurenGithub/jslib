6164f42965003cc643387a5dd496db04f675ad68
guren <guren_d@hotmail.com>
2016-10-08 16:30:39 +0800

    1
    1

--- xUtils/xUtils.js
+++ xUtils/xUtils.js
@@ -19,7 +19,7 @@ if (!document.getElementsByClassName) {
 
 var xUtils = (function() {
   var methods = {
-    isAutoLoad:true,
+    isAutoLoad: true,
     getWidth: function(id, cssWidth) {
       var $$ = null;
       if (typeof id == "string") {
@@ -39,7 +39,7 @@ var xUtils = (function() {
       } else {
         dom.attachEvent('on' + type, function() {
           fn.call(dom, arguments);
-          
+
         });
       }
     },
@@ -68,10 +68,10 @@ var xUtils = (function() {
         }
       }
     },
-getElementsByClass:function(dom,className){
+    getElementsByClass: function(dom, className) {
 
-  var eles=[];
-   if (dom.className.indexOf(className) > -1) {
+      var eles = [];
+      if (dom.className.indexOf(className) > -1) {
         return dom;
       }
       var nodes = dom.children;
@@ -84,65 +84,88 @@ getElementsByClass:function(dom,className){
       }
 
       return eles;
-}
-    ,
-    foreach:function(array,onEach){
-      for (var k = array.length - 1; k >= 0; k--)
-          {
-              onEach(array[k],k) 
-          }
+    },
+    foreach: function(array, onEach) {
+      for (var k = array.length - 1; k >= 0; k--) {
+        onEach(array[k], k)
+      }
 
     },
-    clearNode:function(dom){
+    clearNode: function(dom) {
 
-      var children=dom.children;
+      var children = dom.children;
       for (var i = 0; i < children.length; i++) {
-        
-          dom.removeChild(children[i])
+
+        dom.removeChild(children[i])
       }
     },
-    getWinSize:function(){
+    getWinSize: function() {
 
 
-              var width=0;
-              var height=0;
-                    if(window.innerHeight!= undefined){
-                        height= window.innerHeight;
-                        width= window.innerWidth;
-                    }
-                    else{
-                        var B= document.body, D= document.documentElement;
-                        height= Math.min(D.clientHeight, B.clientHeight);
+      var width = 0;
+      var height = 0;
+      if (window.innerHeight != undefined) {
+        height = window.innerHeight;
+        width = window.innerWidth;
+      } else {
+        var B = document.body,
+          D = document.documentElement;
+        height = Math.min(D.clientHeight, B.clientHeight);
 
-                        width= Math.min(D.clientWidth, B.clientWidth);
-                    }
-                    return {width:width,height:height};
+        width = Math.min(D.clientWidth, B.clientWidth);
+      }
+      return {
+        width: width,
+        height: height
+      };
     },
-    getWinScroll:function(){
-     var scrollTop = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
+    getWinScroll: function() {
+      var scrollTop = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
       var scrollLeft = document.documentElement.scrollLeft || window.pageXOffset || document.body.scrollLeft;
-      return {left:scrollLeft,top:scrollTop};
+      return {
+        left: scrollLeft,
+        top: scrollTop
+      };
     },
-    getOffset:function(el){
-      var scrollX = document.documentElement.scrollLeft || document.body.scrollLeft; 
-      var scrollY = document.documentElement.scrollTop || document.body.scrollTop; 
+    getOffset: function(el) {
+      var scrollX = document.documentElement.scrollLeft || document.body.scrollLeft;
+      var scrollY = document.documentElement.scrollTop || document.body.scrollTop;
 
-var offset=el.getBoundingClientRect();
-      return {left:offset.left+scrollX,top:scrollY+offset.top};
-   },
-    getMousePos:function(event) { 
-      var e = event || window.event; 
-      var scrollX = document.documentElement.scrollLeft || document.body.scrollLeft; 
-      var scrollY = document.documentElement.scrollTop || document.body.scrollTop; 
-      var x = e.pageX || e.clientX + scrollX; 
-      var y = e.pageY || e.clientY + scrollY; 
-      return { 'x': x, 'y': y }; 
+      var offset = el.getBoundingClientRect();
+      return {
+        left: offset.left + scrollX,
+        top: scrollY + offset.top
+      };
     },
-     getSelector:function(selectorId) {
+    getMousePos: function(event) {
+      var e = event || window.event;
+      var scrollX = document.documentElement.scrollLeft || document.body.scrollLeft;
+      var scrollY = document.documentElement.scrollTop || document.body.scrollTop;
+      var x = e.pageX || e.clientX + scrollX;
+      var y = e.pageY || e.clientY + scrollY;
+      return {
+        'x': x,
+        'y': y
+      };
+    },
+    getSelector: function(selectorId) {
 
-    return typeof selectorId == 'object' ? selectorId : document.getElementById(selectorId);
+      return typeof selectorId == 'object' ? selectorId : document.getElementById(selectorId);
 
-  }
+    },
+    loadJs: function(url, onComplete) {
+      var _doc = document.getElementsByTagName('head')[0];
+      var script = document.createElement('script');
+      script.setAttribute('type', 'text/javascript');
+      script.setAttribute('src', url);
+      _doc.appendChild(script);
+      script.onload = script.onreadystatechange = function() {
+        if (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete') {
+          onComplete && onComplete();
+        }
+        script.onload = script.onreadystatechange = null;
+      }
+    }
   }
   return methods;
 })()
