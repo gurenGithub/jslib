8d1b09b79d6af970d96f9544301f79a89cb71cf7
guren <guren_d@hotmail.com>
2016-12-01 14:56:12 +0800

    1
    1

--- H5/xUtils/src/js/ajax.js
+++ H5/xUtils/src/js/ajax.js
@@ -2,94 +2,93 @@
     window.xUtils = {};
 }
 
-window.xUtils.ajax = (function () {
+window.xUtils.ajax = (function() {
 
 
     var members = {
 
-         defaultSaveOpts:function(opts){
+        defaultSaveOpts: function(opts) {
 
 
-                var message = opts.message || '数据保存中...';
-                if (opts) {
-                    opts.beforeSend = function ()
-                    {
-                        utils.dialog.wait(message);
-                    }
-                    opts.complete = function () {
-                        utils.dialog.closeWait();
-                    }
+            var message = opts.message || '数据保存中...';
+            if (opts) {
+                opts.beforeSend = function() {
+                    utils.dialog.wait(message);
                 }
-                return opts;
-            },
-            defaultListOpts:function(opts){
-
-                if (opts) {
-                    opts.beforeSend = function () {
-                        utils.dialog.loadding();
-                    }
-                    opts.complete = function () {
-                        utils.dialog.closeLoadding();
-                    }
+                opts.complete = function() {
+                    utils.dialog.closeWait();
                 }
-            },
-        post: function (url, data, onCustomeOpts)
-        {
+            }
+            return opts;
+        },
+        defaultListOpts: function(opts) {
+
+            if (opts) {
+                opts.beforeSend = function() {
+                    utils.dialog.loadding();
+                }
+                opts.complete = function() {
+                    utils.dialog.closeLoadding();
+                }
+            }
+        },
+        post: function(url, data, onCustomeOpts) {
             var opts = {
                 url: url,
                 type: 'post',
                 data: data,
                 dataType: 'json',
                 timeout: 20e3,
-                success: function (data) {
+                success: function(data) {
 
                     if (onCustomeOpts && onCustomeOpts.onSuccess) {
                         onCustomeOpts.onSuccess(data);
                     }
                 },
-                error: function (e) {
-                 
+                error: function(e) {
+
 
                     var message = e.responseText;
                     if (onCustomeOpts && onCustomeOpts.onError) {
                         onCustomeOpts.onError(message);
                     }
                 },
-                before: function () {
+                before: function() {
                     if (onCustomeOpts && onCustomeOpts.onBefore) {
                         onCustomeOpts.onBefore();
                     }
-                }, complete: function () {
+                },
+                complete: function() {
                     if (onCustomeOpts && onCustomeOpts.onComplete) {
                         onCustomeOpts.onComplete();
                     }
                 }
             };
-            
-            for(var key in onCustomeOpts){
-                opts[key]=onCustomeOpts[key];
+
+            for (var key in onCustomeOpts) {
+                opts[key] = onCustomeOpts[key];
             }
             $.ajax(opts);
         },
-        save:function (url, data, opts) {
+        save: function(url, data, opts) {
 
-            if(!opts){
+            if (!opts) {
                 opts = {};
                 opts.onSuccess = onSuccess;
             }
             if (!opts.onBefore) {
 
-                opts.onBefore = function () {
+                opts.onBefore = function() {
 
                     if (xUtils && xUtils.dialog) {
                         xUtils.dialog.wait(opts.message || '请等待。。。');
-                        
+
                     }
                 };
 
             }
             if (!opts.onComplete) {
-                opts.onComplete = function () {
+                opts.onComplete = function() {
                     if (xUtils && xUtils.dialog) {
                         xUtils.dialog.closeWait();
                     }
@@ -97,24 +96,35 @@ window.xUtils.ajax = (function () {
             }
             if (!opts.onError) {
 
-                opts.onError = function (e) {
+                opts.onError = function(e) {
                     if (xUtils && xUtils.dialog) {
                         xUtils.dialog.alert();
                     }
                 };
             }
-           
+
             this.post(url, data, opts);
         },
-        list: function (url, data, opts)
-        {
+        saveObj: function(url, data, opts) {
+
+            if (!opts) {
+                opts = {};
+            }
+            opts.type = 'post';
+            opts.data = JSON.stringify(data);
+            opts.dataType = "json";
+            opts.contentType = 'application/json';
+            this.save(url, data, opts);
+        }
+        ,
+        list: function(url, data, opts) {
             if (!opts) {
                 opts = {};
                 opts.onSuccess = onSuccess;
             }
             if (!opts.onBefore) {
 
-                opts.onBefore = function () {
+                opts.onBefore = function() {
 
                     if (xUtils && xUtils.dialog) {
                         xUtils.dialog.loadding();
@@ -124,7 +134,7 @@ window.xUtils.ajax = (function () {
 
             }
             if (!opts.onComplete) {
-                opts.onComplete = function () {
+                opts.onComplete = function() {
                     if (xUtils && xUtils.dialog) {
                         xUtils.dialog.closeLoadding();
                     }
@@ -132,7 +142,7 @@ window.xUtils.ajax = (function () {
             }
             if (!opts.onError) {
 
-                opts.onError = function (e) {
+                opts.onError = function(e) {
                     if (xUtils && xUtils.dialog) {
                         xUtils.dialog.alert();
                     }
