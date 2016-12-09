ce1cd6fe77a34cc55dd2dd45418718255a945d70
guren <guren_d@hotmail.com>
2016-12-09 17:48:17 +0800

    1
    1

--- H5/xUtils/dist/js/xUtils.js
+++ H5/xUtils/dist/js/xUtils.js
@@ -1125,3 +1125,20 @@ if (typeof $ !== 'undefined') {
 	}
 
 };
+
+if (typeof $ !== 'undefined') {
+	document.addEventListener('touchmove', function(e) {
+		e.preventDefault();
+	}, false);
+	$.fn.tmpl = function(tmplId,data,appendFunc) {
+
+          var $me=$(this);
+          var html = template(tmplId, data);
+          if(!appendFunc){
+          	      $me.append(html);
+          }else{
+          	appendFunc.call(this,html);
+          }
+       
+	};
+};
